e5e5ae50e69d76f7017c07fd8ad68a1e0255f926
vishvadcodezinnovates <vishva.dcodezinnovates@gmail.com>
2025-04-17 12:17:29 -0500

    update requirements.txt

--- WaterForceProject/settings.py
+++ WaterForceProject/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'openai',
     'admin_interface',
     'colorfield',
     'WaterForceUserApp',
